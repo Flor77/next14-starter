49e134df31287384172242804aa3b7ad450a4927
Flor77 <budaflorin@yahoo.com>
2024-01-12 08:38:25 +0200

    add new logo img

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,13 +2,14 @@ import styles from "./navbar.module.css";
 import Links from "./links/Links";
 import Link from "next/link";
 import { auth } from "@/lib/auth";
+import Image from "next/image";
 
 const Navbar = async () => {
   const session = await auth();
   return (
     <div className={styles.container}>
       <Link href="/" className={styles.logo}>
-        Logo
+        <Image src="/FB_3.png" alt="" width={80} height={80} />
       </Link>
       <div>
         <Links session={session} />
